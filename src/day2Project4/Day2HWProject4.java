d55b225f111188ff711edc938b7f5fabba93ac79
nasim <nasim@10.0.0.10>
2023-04-08 02:29:09 -0400

    Added printInfo()

--- src/day2Project4/Day2HWProject4.java
+++ src/day2Project4/Day2HWProject4.java
@@ -5,6 +5,14 @@ public class Day2HWProject4 {
 	public static void main(String[] args) {
 		
 		System.out.println("Day2 HW project4");
+		
+		
+	}
+	 
+	public void printInfo() {
+		
+		System.out.println("this is day2 Project4 Info");
+		
 	}
 
 }
